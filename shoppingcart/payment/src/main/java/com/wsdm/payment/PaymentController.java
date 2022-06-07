cea50f65977f1c1985afbf47a55d7b18d9ef57b3
Nikos Gavalas <gavalnick@gmail.com>
2022-06-07 20:20:38 +0200

    refactor payment service

--- shoppingcart/payment/src/main/java/com/wsdm/payment/PaymentController.java
+++ shoppingcart/payment/src/main/java/com/wsdm/payment/PaymentController.java
@@ -2,7 +2,6 @@ package com.wsdm.payment;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -16,41 +15,15 @@ public class PaymentController {
     @Autowired
     PaymentService paymentService;
 
-    @PostMapping(path="pay/{user_id}/{order_id}/{amount}")
-    public ResponseEntity pay(@PathVariable("user_id") Integer userId, @PathVariable("order_id") Integer orderId, @PathVariable("amount") Integer amount) {
-        boolean completed = paymentService.makePayment(userId, orderId, amount);
-        if (!completed) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping(path="cancel/{user_id}/{order_id}")
-    public ResponseEntity cancel(@PathVariable("user_id") Integer userId, @PathVariable("order_id") Integer orderId) {
-        boolean completed = paymentService.cancelPayment(userId, orderId);
-        if (!completed) {
-            ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping(path="status/{user_id}/{order_id}")
-    public Object getStatus(@PathVariable("user_id") Integer userId, @PathVariable("order_id") Integer orderId) {
-        return paymentService.getPaymentStatus(userId, orderId);
-    }
-
     @PostMapping(path="create_user")
     public Map<String, Integer> registerUser() {
+        // TODO: return JSON instead of just a single int
         return Map.of("user_id", paymentService.registerUser());
     }
 
     @PostMapping(path="add_funds/{user_id}/{amount}")
-    public Object addFunds(@PathVariable("user_id") Integer userId, @PathVariable("amount") Integer amount) {
-        if (amount <= 0){
-            return ResponseEntity.badRequest().build();
-        }
-        boolean completed =  paymentService.addFunds(userId, amount);
-        return Map.of("done", completed);
+    public boolean addFunds(@PathVariable("user_id") Integer userId, @PathVariable("amount") Integer amount) {
+        return paymentService.addFunds(userId, amount);
     }
 
     @GetMapping(path="find_user/{user_id}")
