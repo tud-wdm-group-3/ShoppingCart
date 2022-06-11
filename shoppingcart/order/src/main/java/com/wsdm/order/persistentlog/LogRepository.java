6ace677daad7631f522d768d0edf22afd9687ec8
jesseharte <38751829+jesseharte@users.noreply.github.com>
2022-06-11 21:06:14 +0200

    Create persistent logs

---
+++ shoppingcart/order/src/main/java/com/wsdm/order/persistentlog/LogRepository.java
@@ -0,0 +1,11 @@
+package com.wsdm.order.persistentlog;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LogRepository extends JpaRepository<Log, Integer> {
+    List<Log> findByMapName(String mapName);
+}
