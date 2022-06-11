6a6e64a3459ae180c7163d18a8d0ead5a4ddd04a
Theodoros Veneti <tvenetis@button-networks.com>
2022-06-11 16:10:29 +0200

    -adjust order wrapper controller to do manual discovery of relevant services and decide appropriate instance
    -modify feign client to delegate request to correct instance based on id passed from controller
    -add needed annotations to ensure all services register with eureka and are discoverable
    -start working on the payment wrapper

--- shoppingcart/orderwrapper/src/main/java/com/wdsm/orderwrapper/OrderClient.java
+++ shoppingcart/orderwrapper/src/main/java/com/wdsm/orderwrapper/OrderClient.java
@@ -1,15 +1,33 @@
 package com.wdsm.orderwrapper;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.openfeign.FeignClientBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name="order",path="orders")
-public interface OrderClient {
+@Component
+public class OrderPartitionClient {
+    FeignClientBuilder feignClientBuilder;
+    public OrderPartitionClient(@Autowired ApplicationContext applicationContext){
+        this.feignClientBuilder=new FeignClientBuilder(applicationContext);
+    }
 
-    @PostMapping("/create/{userId}")
-    String clientCreate(@PathVariable(name="userId") int userId);
+    interface CustomCall {
+        @PostMapping("orders/create/{userId}")
+        public String clientCreate(@PathVariable(name = "userId") int userId);
+    }
+    public String delegateClientCreate(int userid,int partitionId){
+        return delegate(partitionId).clientCreate(userid);
+    }
 
+    public CustomCall delegate(int partitionId){
+        String serviceName="order";
+        if(partitionId>0){
+            serviceName+="-"+partitionId;
+        }
+        return this.feignClientBuilder.forType(CustomCall.class,serviceName).build();
+    }
 
 }
